e8d0484bf6239b265fdd97cfb623272378506d99
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2025-03-26 09:46:35 +0000

    Add challenge instructions

--- lib/battle_of_chars.py
+++ lib/battle_of_chars.py
@@ -1,4 +1,8 @@
 # Coach code challenge for demonstration
 
 def battle_of_chars(string_one, string_two):
+    # Compare 2 strings and return the one that scores higher.
+    # Rules: Each letter has a score, A => 1, B => 2 etc.
+    
+    # code below
     pass
