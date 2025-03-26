321c72e1fa757144661f796953fc683f50a727eb
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2025-03-26 09:47:27 +0000

    Skip coach tests

--- tests/test_battle_of_chars.py
+++ tests/test_battle_of_chars.py
@@ -1,6 +1,6 @@
 from lib.battle_of_chars import battle_of_chars
 
-def test_battle_of_chars():
+def xtest_battle_of_chars():
     assert battle_of_chars("AAA", "Z") == "Z"
     assert battle_of_chars("ONE", "TWO") == "TWO"
     assert battle_of_chars("ONE", "NEO") == "Tie!"
